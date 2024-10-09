8bcd6cd1aaad7e4d1100aed144704dfdeb30c4ac
Annastasia-Seymour <stasia1seymour@gmail.com>
2024-10-09 22:11:42 +0200

    Added an image file

--- asynchrony_challenge/src/App.jsx
+++ asynchrony_challenge/src/App.jsx
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
-
 function App(){
-
+fetch('')
   return (
     <div className='App-header'>
         <h1>Hello</h1>
+        <h2>I am a banana</h2>
+        
+        
     </div>
   )
 }
